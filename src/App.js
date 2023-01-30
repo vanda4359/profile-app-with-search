a33784785d7bb940fdfe44a4fa31079a6269317f
Van Dang Anh <vandait996@gmail.com>
2023-01-30 16:26:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Profile from "./components/profile/Profile";
+
+function App() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/profile/Profile-data.js
@@ -0,0 +1,32 @@
+export const profileData = [
+    {
+        id: 1,
+        name: "Susan B",
+        job: "JavaScript Developer",
+        img: "https://i.ibb.co/7rcbH2C/profile4.png",
+    },
+    {
+        id: 2,
+        name: "John Doe",
+        job: "JavaScript Developer",
+        img: "https://i.ibb.co/7rcbH2C/profile4.png",
+    },
+    {
+        id: 3,
+        name: "Sarah K",
+        job: "JavaScript Developer",
+        img: "https://i.ibb.co/7rcbH2C/profile4.png",
+    },
+    {
+        id: 4,
+        name: "Zino A",
+        job: "JavaScript Developer",
+        img: "https://i.ibb.co/7rcbH2C/profile4.png",
+    },
+    {
+        id: 5,
+        name: "Chioma A",
+        job: "JavaScript Developer",
+        img: "https://i.ibb.co/7rcbH2C/profile4.png",
+    },
+]
